fa7133a2abada997bc9af931f78bf0945f9733e9
Caio Bomfim <108744471+caiob2811@users.noreply.github.com>
2024-10-26 21:09:25 -0300

    subindo new version

--- revisao/exercicios/firbonacci.js
+++ revisao/exercicios/firbonacci.js
@@ -1,26 +1,3 @@
-/* function firbonacciGenerator(n) {
-
-    let saida = []
-
-    let a = 0
-    let b = 1
-
-    while (a <= n) {
-        saida.push(a)
-
-        let soma = a + b
-        a = b
-        b = soma
-    }
-
-
-    console.log(saida)
-
-}
-
-firbonacciGenerator(144)
- */
-
 function firbonacciGenerator(n) {
     let output = []
     let a = 0
